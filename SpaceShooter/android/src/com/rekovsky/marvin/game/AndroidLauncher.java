486e2b6641d25caaf53a4f18dac6d3c323a007d8
Marvin Rekovsky <rekovsky@stud.hs-heilbronn.de>
2018-05-26 22:25:22 +0200

    Created new develop branch

--- SpaceShooter/android/src/com/rekovsky/marvin/game/AndroidLauncher.java
+++ SpaceShooter/android/src/com/rekovsky/marvin/game/AndroidLauncher.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.rekovsky.marvin.game.SpaceShooter;
 
+/**
+ * Gets called first when application starts.
+ */
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
